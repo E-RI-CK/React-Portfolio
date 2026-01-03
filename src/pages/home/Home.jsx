c9dbefb7bd71f15b5b15911d830557100951e247
Andro Valero <74332121+E-RI-CK@users.noreply.github.com>
2026-01-02 23:32:27 -0500

    update: cv file updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
 
   const handleButtonClick = () => {
     const link = document.createElement('a');
-    link.href = '../cv/Andro_Valero_CV__es_.pdf';
+    link.href = '../cv/Andro_Valero_CV.pdf';
     link.download = 'Andro_Valero_CV__es_.pdf';
     document.body.appendChild(link);
     link.click();
